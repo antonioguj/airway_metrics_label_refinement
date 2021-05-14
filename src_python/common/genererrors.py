e0314829c32fd3fb824d951344704818310833d1
antonioguj <antonio.garciauceda89@gmail.com>
2021-05-14 14:53:18 +0200

    implement cylinder blank errors

---
+++ src_python/common/genererrors.py
@@ -0,0 +1,130 @@
+
+from typing import Tuple, List, Dict
+import numpy as np
+
+
+def get_vector_two_points(begin_point: Tuple[float, float, float],
+                          end_point: Tuple[float, float, float]
+                          ) -> Tuple[float, float, float]:
+    return (end_point[0] - begin_point[0],
+            end_point[1] - begin_point[1],
+            end_point[2] - begin_point[2])
+
+def get_point_in_segment(begin_point: Tuple[float, float, float],
+                         end_point: Tuple[float, float, float],
+                         rel_dist_segm: float
+                         ) -> Tuple[float, float, float]:
+    vector_segment = get_vector_two_points(begin_point, end_point)
+    return (begin_point[0] + rel_dist_segm * vector_segment[0],
+            begin_point[1] + rel_dist_segm * vector_segment[1],
+            begin_point[2] + rel_dist_segm * vector_segment[2])
+
+
+def _get_indexes_canditate_inside_blank(point_center: Tuple[float, float, float],
+                                        max_dist: float
+                                        ) -> np.array:
+    min_index_x = int(np.floor(point_center[0] - max_dist))
+    max_index_x = int(np.ceil(point_center[0] + max_dist))
+    min_index_y = int(np.floor(point_center[1] - max_dist))
+    max_index_y = int(np.ceil(point_center[1] + max_dist))
+    min_index_z = int(np.floor(point_center[2] - max_dist))
+    max_index_z = int(np.ceil(point_center[2] + max_dist))
+    indexes_x = np.arange(min_index_x, max_index_x + 1)
+    indexes_y = np.arange(min_index_y, max_index_y + 1)
+    indexes_z = np.arange(min_index_z, max_index_z + 1)
+    return np.stack(np.meshgrid(indexes_x, indexes_y, indexes_z, indexing='ij'), axis=3)
+
+
+def _get_indexes_inside_blank_sphere(indexes_candits_inside_blank: np.ndarray,
+                                     point_center: Tuple[float, float, float],
+                                     diam_sphere: float
+                                     ) -> np.ndarray:
+    # relative distance of candidate indexes to center
+    locs_rel2center_candits = indexes_candits_inside_blank - point_center
+
+    radius_sphere = diam_sphere / 2.0
+
+    # condition for sphere: if distance to center is less than radius
+    is_indexes_inside_blank = np.linalg.norm(locs_rel2center_candits, axis=3) <= radius_sphere
+    # array of ['True', 'False'], with 'True' for indexes that are inside the blank
+
+    return indexes_candits_inside_blank[is_indexes_inside_blank]
+
+
+def _get_indexes_inside_blank_cylinder(indexes_candits_inside_blank: np.ndarray,
+                                       point_center: Tuple[float, float, float],
+                                       vector_axis: Tuple[float, float, float],
+                                       diam_base: float,
+                                       length_axis: float
+                                       ) -> np.ndarray:
+    # relative distance of candidate indexes to center
+    locs_rel2center_candits = indexes_candits_inside_blank - point_center
+
+    norm_vector_axis = np.sqrt(vector_axis.dot(vector_axis))
+    unit_vector_axis = vector_axis / norm_vector_axis
+    radius_base = diam_base / 2.0
+    half_length_axis = length_axis / 2.0
+
+    # conditions for cylinder: 1) if distance to center, parallel to axis, is less than length_axis
+    #                          2) if distance to center, percendicular to axis, is less than radius_base
+    dist_rel2center_parall_axis_candits = np.dot(locs_rel2center_candits, unit_vector_axis)
+
+    is_indexes_inside_blank_parall = dist_rel2center_parall_axis_candits <= half_length_axis
+    is_indexes_inside_blank_perpen = np.sqrt(np.square(np.linalg.norm(locs_rel2center_candits, axis=3), 2)
+                                             - np.square(dist_rel2center_parall_axis_candits, 2)) <= radius_base
+
+    is_indexes_inside_blank = np.logical_and(is_indexes_inside_blank_parall, is_indexes_inside_blank_perpen)
+    # array of ['True', 'False'], with 'True' for indexes that are inside the blank
+
+    return indexes_candits_inside_blank[is_indexes_inside_blank]
+
+
+def _get_max_distance_2center_sphere(diam_sphere: float) -> float:
+    return diam_sphere / 2.0
+
+
+def _get_max_distance_2center_cylinder(diam_base: float, length_axis: float) -> float:
+    radius_base = diam_base / 2.0
+    half_length_axis = length_axis / 2.0
+    return np.sqrt(radius_base**2 + half_length_axis**2)
+
+
+def generate_error_blank_branch(inout_mask: np.ndarray,
+                                point_center: Tuple[float, float, float],
+                                type_blank_shape: str,
+                                params_blank_shape: Dict[str, float]
+                                ) -> np.ndarray:
+    if type_blank_shape not in ['sphere', 'cylinder']:
+        message = 'ERROR: input \'type_blank_shape\' not valid. Options available: [sphere, cylinder]... SKIP'
+        print(message)
+        return inout_mask
+
+    # candidates: subset of mask indexes where to check condition for error shape -> to save time
+    if type_blank_shape == 'sphere':
+        diam_blank = params_blank_shape['diam']
+        max_dist2cen_blank = _get_max_distance_2center_sphere(diam_blank)
+    elif type_blank_shape == 'cylinder':
+        diam_base_blank = params_blank_shape['diam_base']
+        length_axis_blank = params_blank_shape['length_axis']
+        max_dist2cen_blank = _get_max_distance_2center_cylinder(diam_base_blank, length_axis_blank)
+
+    indexes_candits_inside_blank = _get_indexes_canditate_inside_blank(point_center, max_dist2cen_blank)
+    # array of indexes, with dims [num_indexes_x, num_indexes_y, num_indexes_z, 3]
+
+    # get points from the candidate indexes that are inside the blank of the given shape
+    if type_blank_shape == 'sphere':
+        diam_blank = params_blank_shape['diam']
+        indexes_inside_blank = _get_indexes_inside_blank_sphere(indexes_candits_inside_blank, point_center,
+                                                                diam_blank)
+    elif type_blank_shape == 'cylinder':
+        vector_axis = params_blank_shape['vector_axis']
+        diam_base_blank = params_blank_shape['diam_base']
+        length_axis_blank = params_blank_shape['length_axis']
+        indexes_inside_blank = _get_indexes_inside_blank_cylinder(indexes_candits_inside_blank, point_center,
+                                                                  vector_axis, diam_base_blank, length_axis_blank)
+
+    (indexes_x_error, indexes_y_error, indexes_z_error) = np.transpose(indexes_inside_blank)
+
+    # blank error: set '0' to voxels for indexes inside the blank
+    inout_mask[indexes_z_error, indexes_y_error, indexes_x_error] = 0
+    return inout_mask
--- src_python/generate_airway_errors.py
+++ src_python/generate_airway_errors.py
@@ -1,73 +1,10 @@
 
-from typing import Tuple, List
 from collections import OrderedDict
 import argparse
 
 from common.functionutil import *
 from common.filereader import NiftiFileReader, CsvFileReader
-
-
-def get_vector_two_points(point_1: Tuple[float, float, float],
-                          point_2: Tuple[float, float, float]
-                          ) -> Tuple[float, float, float]:
-    return (point_2[0] - point_1[0],
-            point_2[1] - point_1[1],
-            point_2[2] - point_1[2])
-
-
-def get_norm_vector(in_vector: Tuple[float, float, float]) -> float:
-    return np.sqrt(in_vector[0] * in_vector[0] + in_vector[1] * in_vector[1] + in_vector[2] * in_vector[2])
-
-
-def get_loc_between_two_points(point_1: Tuple[float, float, float],
-                               point_2: Tuple[float, float, float],
-                               reldist_p1_p2: float
-                               ) -> Tuple[float, float, float]:
-    vector_p1_p2 = get_vector_two_points(point_1, point_2)
-    return (point_1[0] + reldist_p1_p2 * vector_p1_p2[0],
-            point_1[1] + reldist_p1_p2 * vector_p1_p2[1],
-            point_1[2] + reldist_p1_p2 * vector_p1_p2[2])
-
-
-def get_canditate_indexes_gen_error(loc_center_error: Tuple[float, float, float],
-                                    max_dist_error: float
-                                    ) -> Tuple[np.array]:
-    min_index_x = int(np.floor(loc_center_error[0] - max_dist_error))
-    max_index_x = int(np.ceil(loc_center_error[0] + max_dist_error))
-    min_index_y = int(np.floor(loc_center_error[1] - max_dist_error))
-    max_index_y = int(np.ceil(loc_center_error[1] + max_dist_error))
-    min_index_z = int(np.floor(loc_center_error[2] - max_dist_error))
-    max_index_z = int(np.ceil(loc_center_error[2] + max_dist_error))
-    indexes_x = np.arange(min_index_x, max_index_x + 1)
-    indexes_y = np.arange(min_index_y, max_index_y + 1)
-    indexes_z = np.arange(min_index_z, max_index_z + 1)
-    return np.stack(np.meshgrid(indexes_x, indexes_y, indexes_z, indexing='ij'), axis=3)
-
-
-def generate_error_sphere_shape_inside_branch(inout_mask: np.ndarray,
-                                              loc_center_error: Tuple[float, float, float],
-                                              diam_spher_error: float
-                                              ) -> np.ndarray:
-    # candidates: subset of mask indexes where to check condition for error shape -> to save time
-    indexes_candits_gen_error = get_canditate_indexes_gen_error(loc_center_error,
-                                                                diam_spher_error / 2.0)
-    # array of indexes, with dims [num_indexes_x, num_indexes_y, num_indexes_z, 3]
-
-    # condition for sphere shape -> if relative distance of candidate indexes to center is smaller than radius
-    relpos_2center_candits_gen_error = indexes_candits_gen_error - loc_center_error
-    radius_spher_error = diam_spher_error / 2.0
-
-    is_indexes_inside_error = np.linalg.norm(relpos_2center_candits_gen_error, axis=3) < radius_spher_error
-    # array of ['True', 'False'], with 'True' for indexes that meet the error shape condition
-
-    indexes_generate_error = indexes_candits_gen_error[is_indexes_inside_error]
-    # array with only indexes that do meet the error shape condition
-
-    (indexes_x_error, indexes_y_error, indexes_z_error) = np.transpose(indexes_generate_error)
-
-    # set to '0' the voxels for indexes that belong to error
-    inout_mask[indexes_z_error, indexes_y_error, indexes_x_error] = 0
-    return inout_mask
+from common.genererrors import get_vector_two_points, get_point_in_segment, generate_error_blank_branch
 
 
 def main(args):
@@ -129,7 +66,7 @@ def main(args):
         in_endpoint_z_branches = in_airway_measures['endPoint_z']
 
         in_voxelsize_image = in_images_voxelsize_info[in_casename]
-        in_voxelnorm_image = get_norm_vector(in_voxelsize_image)
+        in_voxelnorm_image = np.linalg.norm(in_voxelsize_image)
 
         num_branches = len(in_air_index_branches)
 
@@ -140,38 +77,39 @@ def main(args):
         if args.random_seed:
             np.random.seed(args.random_seed)
 
-        random_indexes_branches_error = np.random.choice(range(num_branches), num_branches_error, replace=False)
+        indexes_branches_gener_error = np.random.choice(range(num_branches), num_branches_error, replace=False)
 
-        for index_brh in random_indexes_branches_error:
+        for index_brh in indexes_branches_gener_error:
 
-            loc_midpoint_branch = (in_midpoint_x_branches[index_brh],
-                                   in_midpoint_y_branches[index_brh],
-                                   in_midpoint_z_branches[index_brh])
-            loc_begpoint_branch = (in_begpoint_x_branches[index_brh],
-                                   in_begpoint_y_branches[index_brh],
-                                   in_begpoint_z_branches[index_brh])
-            loc_endpoint_branch = (in_endpoint_x_branches[index_brh],
-                                   in_endpoint_y_branches[index_brh],
-                                   in_endpoint_z_branches[index_brh])
+            # mid_point_branch = (in_midpoint_x_branches[index_brh],
+            #                     in_midpoint_y_branches[index_brh],
+            #                     in_midpoint_z_branches[index_brh])
+            begin_point_branch = (in_begpoint_x_branches[index_brh],
+                                  in_begpoint_y_branches[index_brh],
+                                  in_begpoint_z_branches[index_brh])
+            end_point_branch = (in_endpoint_x_branches[index_brh],
+                                in_endpoint_y_branches[index_brh],
+                                in_endpoint_z_branches[index_brh])
 
             inner_diam_branch = in_inner_diam_branches[index_brh] / in_voxelnorm_image
             length_branch = in_air_length_branches[index_brh] / in_voxelnorm_image
 
             # random position along the branch and length, for the generated error
-            random_relpos_error_branch = np.random.random()
-            #random_length_error_branch = np.random.random() * length_branch
-            random_length_error_branch = inner_diam_branch * 3
+            relpos_blank_branch = np.random.random()
+            length_blank_branch = np.random.random() * length_branch
 
-            # vector_direc_branch = get_vector_two_points(loc_begpoint_branch,
-            #                                             loc_endpoint_branch)
+            vector_axis_branch = get_vector_two_points(begin_point_branch, end_point_branch)
 
-            loc_point_error_branch = get_loc_between_two_points(loc_begpoint_branch,
-                                                                loc_endpoint_branch,
-                                                                random_relpos_error_branch)
+            point_bank_branch = get_point_in_segment(begin_point_branch, end_point_branch, relpos_blank_branch)
+            #params_blank_shape = {'diam': 2 * inner_diam_branch}
+            params_blank_shape = {'vector_axis': vector_axis_branch,
+                                  'diam_base': inner_diam_branch,
+                                  'length_axis': length_blank_branch}
 
-            inout_airway_mask = generate_error_sphere_shape_inside_branch(inout_airway_mask,
-                                                                          loc_point_error_branch,
-                                                                          random_length_error_branch)
+            inout_airway_mask = generate_error_blank_branch(inout_airway_mask,
+                                                            point_bank_branch,
+                                                            type_blank_shape='cylinder',
+                                                            params_blank_shape=params_blank_shape)
         # endfor
 
         out_airway_error_mask_file = in_casename + '_airways-errors.nii.gz'
@@ -187,7 +125,7 @@ if __name__ == '__main__':
     parser.add_argument('--inbasedir', type=str, default='.')
     parser.add_argument('--prop_branches_error', type=float, default=0.5)
     parser.add_argument('--random_seed', type=int, default=2017)
-    parser.add_argument('--output_dir', type=str, default='./ResultsTest/')
+    parser.add_argument('--output_dir', type=str, default='./ResultsTest_2/')
     args = parser.parse_args()
 
     args.output_dir = join_path_names(args.inbasedir, args.output_dir)
